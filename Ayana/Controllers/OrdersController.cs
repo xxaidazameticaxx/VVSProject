69cc1e49eb76de334e0203cfe51592da501789ee
lheleg <lheleg1@etf.unsa.ba>
2023-11-30 11:14:15 +0100

    code cleanup

--- Ayana/Controllers/OrdersController.cs
+++ Ayana/Controllers/OrdersController.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Ayana.Data;
 using Ayana.Models;
@@ -20,16 +18,10 @@ namespace Ayana.Controllers
             _context = context;
         }
 
-        // GET: Orders
-        public async Task<IActionResult> Index()
-        {
-            return View(await _context.Orders.ToListAsync());
-        }
-
         public List<List<Product>> GetOrderProducts(List<Order> orders)
         {
             List<List<Product>> orderProducts = new List<List<Product>>();
-
+            // Get the associated products for each order
             foreach (var order in orders)
             {
                 var products = _context.ProductOrders
@@ -39,14 +31,12 @@ namespace Ayana.Controllers
 
                 orderProducts.Add(products);
             }
-
+            // Returns list of product lists for each order
             return orderProducts;
         }
 
-
         public IActionResult UserOrders()
         {
-            var p1 = _context.Users.ToList();
             string userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
 
             // Retrieve user-specific orders based on the CustomerId
@@ -58,55 +48,12 @@ namespace Ayana.Controllers
             List<List<Product>> orderProducts = GetOrderProducts(userOrders);
 
             // Pass the userOrders and orderProducts to the view
-            ViewBag.UserOrders = userOrders.OrderBy(order => order.Rating).ToList(); ;
+            ViewBag.UserOrders = userOrders.OrderBy(order => order.Rating).ToList(); 
             ViewBag.OrderProducts = orderProducts;
 
             // Render the view
             return View();
         }
-
-
-        // GET: Orders/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var order = await _context.Orders
-                .FirstOrDefaultAsync(m => m.OrderID == id);
-            if (order == null)
-            {
-                return NotFound();
-            }
-
-            return View(order);
-        }
-
-        // GET: Orders/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Orders/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("OrderID,CustomerID,PaymentID,IsOrderSent,Rating,TotalAmountToPay,DeliveryDate")] Order order)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(order);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(order);
-        }
-
-
 
         // POST: Orders/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
@@ -121,40 +68,6 @@ namespace Ayana.Controllers
             existingOrder.Rating = order.Rating;
             _context.SaveChanges();
             return Redirect("/Orders/UserOrders");
-        }
-
-        // GET: Orders/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var order = await _context.Orders
-                .FirstOrDefaultAsync(m => m.OrderID == id);
-            if (order == null)
-            {
-                return NotFound();
-            }
-
-            return View(order);
-        }
-
-        // POST: Orders/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var order = await _context.Orders.FindAsync(id);
-            _context.Orders.Remove(order);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool OrderExists(int id)
-        {
-            return _context.Orders.Any(e => e.OrderID == id);
         }
     }
 }
