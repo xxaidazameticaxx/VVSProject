4b9c7de015b91a3d7770c67359cfc2ccc8c2b551
Aida Zametica <azametica1@etf.unsa.ba>
2023-12-08 19:43:05 +0100

    Added tests

---
+++ AyanaTests/ErrorViewModelTests.cs
@@ -0,0 +1,59 @@
+﻿using Ayana.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AyanaTests
+{
+    [TestClass]
+    public class ErrorViewModelTests
+    {
+        // written by : Aida Zametica
+        [TestMethod]
+        public void RequestId_SetAndGet()
+        {
+            // Arrange
+            var errorViewModel = new ErrorViewModel();
+            var expectedRequestId = "123";
+
+            // Act
+            errorViewModel.RequestId = expectedRequestId;
+            var actualRequestId = errorViewModel.RequestId;
+
+            // Assert
+            Assert.AreEqual(expectedRequestId, actualRequestId);
+        }
+
+        // written by : Aida Zametica
+        [TestMethod]
+        public void ShowRequestId_TrueWhenRequestIdSet()
+        {
+            // Arrange
+            var errorViewModel = new ErrorViewModel();
+            errorViewModel.RequestId = "123";
+
+            // Act
+            var showRequestId = errorViewModel.ShowRequestId;
+
+            // Assert
+            Assert.IsTrue(showRequestId);
+        }
+
+        // written by : Aida Zametica
+        [TestMethod]
+        public void ShowRequestId_FalseWhenRequestIdNotSet()
+        {
+            // Arrange
+            var errorViewModel = new ErrorViewModel();
+
+            // Act
+            var showRequestId = errorViewModel.ShowRequestId;
+
+            // Assert
+            Assert.IsFalse(showRequestId);
+        }
+
+    }
+}
---
+++ AyanaTests/ProductSalesTests.cs
@@ -0,0 +1,37 @@
+﻿using Ayana.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AyanaTests
+{
+    [TestClass()]
+    public class ProductSalesTests
+    {
+        // written by : Aida Zametica
+        [TestMethod]
+        public void ProductSalesID_PropertyTest()
+        {
+            var productSales = new ProductSales();
+
+            productSales.ProductSalesID = 1;
+
+            Assert.AreEqual(1, productSales.ProductSalesID);
+        }
+
+        // written by : Aida Zametica
+        [TestMethod]
+        public void Product_PropertyTest()
+        {
+            var productSales = new ProductSales();
+            var product = new Product { ProductID = 3 };
+
+            productSales.Product = product;
+
+            Assert.AreEqual(product, productSales.Product);
+        }
+    }
+}
