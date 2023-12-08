a1b06ac5cb531eddcd7c05d8009b0e0533574d8d
Aida Zametica <azametica1@etf.unsa.ba>
2023-12-08 19:54:42 +0100

    Added tests for DtoRequest

---
+++ AyanaTests/DtoRequestTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AyanaTests
+{
+    using Ayana.Models;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class DtoRequestTests
+    {
+        // written by : Aida Zametica
+        [TestMethod]
+        public void DtoRequest_Validation_Test()
+        {
+            // Arrange
+            var dtoRequest = new DtoRequest
+            {
+                DtoRequestID = 1,
+
+                payment = new Payment(),
+                order = new Order(),
+                discount = new Discount(),
+                product = new Product(),
+                productOrder = new ProductOrder(),
+                productSales = new ProductSales(),
+                cart= new Cart(),
+                customer=new Ayana.Data.ApplicationUser { AccessFailedCount = 1 },
+            };
+
+       
+            Assert.IsNotNull(dtoRequest);
+        }
+    }
+
+}
