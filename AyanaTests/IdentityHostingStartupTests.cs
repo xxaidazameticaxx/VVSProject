3f5b4359d1205b1fa40b0cd30a3dd7eebe2b9523
Ilhan Hasičić <ihasicic1@etf.unsa.ba>
2023-12-11 19:16:15 +0100

    added tests

---
+++ AyanaTests/IdentityHostingStartupTests.cs
@@ -0,0 +1,31 @@
+﻿using Ayana.Areas.Identity;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace AyanaTests
+{
+    [TestClass]
+    public class IdentityHostingStartupTests
+    {
+        // written by: Hasičić Ilhan
+        [TestMethod]
+        public void Configure_ShouldCallConfigureServices()
+        {
+            var hostingEnvironmentMock = new Mock<IWebHostEnvironment>();
+            var configurationMock = new Mock<IConfiguration>();
+
+            var builderMock = new Mock<IWebHostBuilder>();
+            builderMock.Setup(b => b.ConfigureServices(It.IsAny<Action<WebHostBuilderContext, IServiceCollection>>()))
+                       .Callback<Action<WebHostBuilderContext, IServiceCollection>>(action => action(null, new ServiceCollection()));
+
+            var identityHostingStartup = new IdentityHostingStartup();
+
+            identityHostingStartup.Configure(builderMock.Object);
+
+            builderMock.Verify(b => b.ConfigureServices(It.IsAny<Action<WebHostBuilderContext, IServiceCollection>>()), Times.Once);
+        }
+    }
+}
--- AyanaTests/ProgramTests.cs
+++ AyanaTests/ProgramTests.cs
@@ -8,56 +8,13 @@ namespace AyanaTests
     [TestClass]
     public class ProgramTests
     {
-        private StringWriter consoleOutput;
-
-        [TestInitialize]
-        public void Initialize()
-        {
-            // Redirect console output to a StringWriter
-            consoleOutput = new StringWriter();
-            Console.SetOut(consoleOutput);
-        }
-
-        [TestCleanup]
-        public void Cleanup()
-        {
-            // Reset console output
-            consoleOutput.Dispose();
-        }
-
-        [TestMethod]
-        public async Task Main_ShouldCallBuildAndRun()
-        {
-            // Arrange
-            var originalOut = Console.Out;
-            var args = new string[] { };
-
-            try
-            {
-                // Act
-                await Task.Run(() => Program.Main(args));
-
-                // Allow some time for the asynchronous Main method to complete
-                Thread.Sleep(1000);
-
-                // Assert
-                var output = consoleOutput.ToString();
-                Assert.IsTrue(output.Contains("Application started."), "Build and Run methods should be called.");
-            }
-            finally
-            {
-                // Restore console output
-                Console.SetOut(originalOut);
-            }
-        }
+        // written by: Hasičić Ilhan
         [TestMethod]
         public void CreateHostBuilder_ShouldReturnValidHostBuilder()
         {
-            // Arrange & Act
             var args = new string[] { };
             var hostBuilder = Program.CreateHostBuilder(args);
 
-            // Assert
             Assert.IsNotNull(hostBuilder);
             Assert.IsInstanceOfType(hostBuilder, typeof(IHostBuilder));
         }
