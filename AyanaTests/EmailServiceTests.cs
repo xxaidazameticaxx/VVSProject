dd50713a407bb4f88ca6a40ad46d6600307bfd9e
Ilhan Hasičić <ihasicic1@etf.unsa.ba>
2023-12-11 17:58:44 +0100

    implemented tests for EmailService class

---
+++ AyanaTests/EmailServiceTests.cs
@@ -0,0 +1,119 @@
+﻿using Ayana.MailgunService;
+using Microsoft.Extensions.Caching.Memory;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AyanaTests
+{
+    [TestClass]
+    public class EmailServiceTests
+    {
+        // written by: Ilhan Hasičić
+        [TestMethod]
+        public void SendVerificationCode_ShouldCallSendEmailToCustomer()
+        {
+            var mailgunServiceMock = new Mock<IMailgunService>();
+            var emailService = new EmailService(mailgunServiceMock.Object);
+
+            emailService.SendVerificationCode("test@example.com");
+
+            mailgunServiceMock.Verify(
+                x => x.SendEmail(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()),
+                Times.Once);
+        }
+
+        // written by: Ilhan Hasičić
+        [TestMethod]
+        public void GetVerificationCode_ShouldReturnNullWhenCacheIsEmpty()
+        {
+            var emailService = new EmailService(Mock.Of<IMailgunService>());
+
+            var result = emailService.GetVerificationCode();
+
+            Assert.IsNull(result);
+        }
+
+        // written by: Ilhan Hasičić
+        [TestMethod]
+        public void VerifyCode_ShouldReturnTrueForValidCode()
+        {
+            var emailService = new EmailService(Mock.Of<IMailgunService>());
+            var code = emailService.GenerateCode();
+            emailService.SaveVerificationCode(code, TimeSpan.FromMinutes(10));
+
+            var result = emailService.VerifyCode("test@example.com", code);
+
+            Assert.IsTrue(result);
+        }
+
+        // written by: Ilhan Hasičić
+        [TestMethod]
+        public void VerifyCode_ShouldReturnFalseForInvalidCode()
+        {
+            var emailService = new EmailService(Mock.Of<IMailgunService>());
+            emailService.SaveVerificationCode("validCode", TimeSpan.FromMinutes(10));
+
+            var result = emailService.VerifyCode("test@example.com", "invalidCode");
+
+            Assert.IsFalse(result);
+        }
+
+        // written by: Ilhan Hasičić
+        [TestMethod]
+        public void GenerateCode_ShouldReturn4DigitCode()
+        {
+            var emailService = new EmailService(Mock.Of<IMailgunService>());
+
+            var code = emailService.GenerateCode();
+
+            Assert.IsTrue(int.TryParse(code, out int codeValue) && codeValue >= 1000 && codeValue <= 9999);
+        }
+
+        [TestMethod]
+        public void SaveCodeToCache_ShouldCacheCodeForSpecifiedEmail()
+        {
+            // Arrange
+            var emailService = new EmailService(Mock.Of<IMailgunService>());
+            var email = "test@example.com";
+            var code = "1234";
+
+            // Act
+            emailService.SaveCodeToCache(email, code);
+
+            // Use reflection to access the private Cache field
+            var cacheField = typeof(EmailService).GetField("Cache", BindingFlags.Instance | BindingFlags.NonPublic);
+            var cache = (IMemoryCache)cacheField.GetValue(emailService);
+
+            // Assert
+            var cachedCode = cache.Get(email) as string;
+            Assert.AreEqual(code, cachedCode);
+        }
+
+        [TestMethod]
+        public void SaveCodeToCache_ShouldCacheCodeForSpecifiedEmail2()
+        {
+            // Arrange
+            var emailService = new EmailService(Mock.Of<IMailgunService>());
+            var email = "test@example.com";
+            var code = "1234";
+
+            // Act
+            emailService.SaveCodeToCache(email, code);
+
+            // Use reflection to access the private Cache field
+            var cacheField = typeof(EmailService).GetField("Cache", BindingFlags.Instance | BindingFlags.NonPublic);
+
+            var cache = (IMemoryCache)cacheField.GetValue(emailService);
+            var cachedCode = cache.Get(email) as string;
+            Assert.AreEqual(code, cachedCode);
+        }
+
+    }
+
+}
+
